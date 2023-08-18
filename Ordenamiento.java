f72715db5b9b7ad7e2f79bfca8d7131a816cbfbf
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 19:01:29 -0500

    pido que ingrese los numero y definimos la cantidad de numeros

--- Ordenamiento.java
+++ Ordenamiento.java
@@ -7,6 +7,13 @@ public class Ordenamiento {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, ingresa 5 números al azar:");
+
+        int[] numbers = new int[5];
+        
+        
+        
     }
     
 }
